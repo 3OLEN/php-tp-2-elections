82f2e16b36f47778e393928ea3a7bc4ad2111504
Billy VILLENA <coding@devrendal.fr>
2024-12-08 11:30:03 +0100

    🩹🍱 Chargement des assets
    * Utilisation du chemin absolu

--- templates/layout/head.php
+++ templates/layout/head.php
@@ -7,7 +7,7 @@ echo <<<HTML
 <head>
     <meta charset="UTF-8">
     <title>3OLEN - Élections</title>
-    <link rel="stylesheet" href="public/styles/app.css">
-    <link rel="icon" type="image/x-icon" href="public/images/favicon.ico">
+    <link rel="stylesheet" href="/public/styles/app.css">
+    <link rel="icon" type="image/x-icon" href="/public/images/favicon.ico">
 </head>
 HTML;
